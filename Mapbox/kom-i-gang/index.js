9ddc41c3eb2d180a9589c96f3fb6a7348a21e6e3
Catharina <catharina.sorum@outlook.com>
2020-01-07 09:56:45 +0100

    added center and zoom

--- Mapbox/kom-i-gang/index.js
+++ Mapbox/kom-i-gang/index.js
@@ -4,5 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2F0aGFyaW5hc29ydW0iLCJhIjoiY2s1M2x1MTFxMDl6a
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 2,
+    center: [13.401067, 52.519087] // Er omvendt av det google maps har
 });
