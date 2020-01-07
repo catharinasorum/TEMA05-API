e0caecfe0eaa81558525997479d103e2930c199c
Catharina <catharina.sorum@outlook.com>
2020-01-07 09:46:07 +0100

    added mapbox

---
+++ Mapbox/kom-i-gang/index.js
@@ -0,0 +1,8 @@
+
+// Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2F0aGFyaW5hc29ydW0iLCJhIjoiY2s1M2x1MTFxMDl6azNnbzRtNGZwZXRpOSJ9.cKX9PjWKrPIY6Pa_jQWpMg';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
