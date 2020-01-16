bce53ef611bc4cbf91b9d4d45723c0f6227b0f23
Catharina <catharina.sorum@outlook.com>
2020-01-16 09:52:09 +0100

    hello

--- Highcharts/chart-fra-scratch/index.js
+++ Highcharts/chart-fra-scratch/index.js
@@ -1,17 +1,27 @@
+// Bruk helst dette oppsettet på charts.
 
 const options = {
-    title: {
-        text: "Mitt diagram"
-    },
     chart: {
         renderTo: "container",
         type: "column"
     },
+    title: {
+        text: "Mitt diagram"
+    },
     series: [
         {
-            
+            name: "Allan Ball",
+            data: [20, 15, 5]
         }
-    ]
+    ],
+    xAxis: {
+        categories: ["Scoringer", "Assists", "Gule kort"]
+    },
+    yAxis: {
+        title: {
+            text: ""
+        }
+    }
 };
 
 const chart = new Highcharts.Chart(options);
