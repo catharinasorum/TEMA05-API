8efa0aebf7b87f8ca28be3dfb64d03a649b58f18
Catharina <catharina.sorum@outlook.com>
2020-01-16 09:46:28 +0100

    added highcharts

---
+++ Highcharts/chart-fra-scratch/index.js
@@ -0,0 +1,17 @@
+
+const options = {
+    title: {
+        text: "Mitt diagram"
+    },
+    chart: {
+        renderTo: "container",
+        type: "column"
+    },
+    series: [
+        {
+            
+        }
+    ]
+};
+
+const chart = new Highcharts.Chart(options);
