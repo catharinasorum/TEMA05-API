0f397e3d3ce662d87db26974ef3afa1bb3fbddb1
Catharina <catharina.sorum@outlook.com>
2020-01-16 10:29:35 +0100

    added fotballplayers and buttons

--- Highcharts/chart-fra-scratch/index.js
+++ Highcharts/chart-fra-scratch/index.js
@@ -6,12 +6,12 @@ const options = {
         type: "column"
     },
     title: {
-        text: "Mitt diagram"
+        text: "Fotball-statistikk"
     },
     series: [
         {
-            name: "Allan Ball",
-            data: [20, 15, 5]
+            name: "Eksempel spiller",
+            data: [10, 5, 2]
         }
     ],
     xAxis: {
@@ -25,3 +25,41 @@ const options = {
 };
 
 const chart = new Highcharts.Chart(options);
+
+/*// Knappen skal bytte spiller når den trykkes på
+const btn = document.querySelector("#btn");
+btn.onclick = () => {
+    chart.title.update(
+        {text: "Fotball-statistikk"}
+    );
+    chart.series[0].update(
+        {
+            name: "Tom Lund",
+            data: [20, 30, 40]
+        }
+    );
+};
+*/
+
+// Tom
+const btnTom = document.querySelector("#btnTom");
+btnTom.onclick = () => {
+
+    chart.series[0].update(
+        {
+            name: "Tom Lund",
+            data: [20, 30, 40]
+        }
+    );
+};
+
+// Alan
+const btnAlan = document.querySelector("#btnAlan");
+btnAlan.onclick = () => {
+    chart.series[0].update(
+        {
+            name: "Alan Ball",
+            data: [20, 15, 5]
+        }
+    );
+};
